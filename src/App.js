8922888c31b08f559188c73d788783a56bf37751
nykachi <nykachi@gmail.com>
2021-12-27 20:58:02 -0500

    3

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Feed from './components/Feed';
+import Navbar from './components/Navbar';
+import { Box } from '@mui/material';
+import VideoDetail from './components/VideoDetail';
+import SearchFeed from './components/SearchFeed';
+import './app.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      Hello Jay
-    </div>
+    <Router>
+      <Box sx={{ p: 1 }}>
+        <Navbar />
+        <Switch>
+          <Route exact path='/' component={Feed} />
+          <Route path='/video-details/:id' component={VideoDetail} />
+          <Route path='/search' component={SearchFeed} />
+        </Switch>
+      </Box>
+    </Router>
   );
-}
+};
 
 export default App;
